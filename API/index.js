e1370961a98f29d30d5a7f0d0e8a1c3ddf6e1d8f
Samkelo <sammpokela@gmail.com>
2023-09-07 12:30:45 +0200

    update

--- API/index.js
+++ API/index.js
@@ -13,35 +13,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.listen(5000, () => console.log('Server running at http://localhost:5000'));
 //static
-app.use(express.static('./static'))
-// Middleware - APplication level
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Expose-Headers", "Authorization");
-    next();
-  });
-  app.use(
-    express.static('./static'),
-    express.urlencoded({
-        extended: false
-    }),
-    cookieParser(),
-    cors(),
-    routes
-)
-  // Handling all errors
-  app.use(ErrorHandling);
-  // Server
-routes.get('^/$|/challenger',(req, res)=>{
-    res.sendFile(path.resolve(__dirname,
-        "./static/HTML/index.html"))
-})
-//Handling all errors using middleware
-app.use(ErrorHandling)
-app.listen(port, ()=>{
-    console.log(`The server is running on port ${port}`);
-})
+// app.use(express.static('./static'))
+// // Middleware - APplication level
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "*");
+//     res.header("Access-Control-Request-Methods", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Expose-Headers", "Authorization");
+//     next();
+//   });
+//   app.use(
+//     express.static('./static'),
+//     express.urlencoded({
+//         extended: false
+//     }),
+//     cookieParser(),
+//     cors(),
+//     routes
+// )
+//   // Handling all errors
+//   app.use(ErrorHandling);
+//   // Server
+// routes.get('^/$|/challenger',(req, res)=>{
+//     res.sendFile(path.resolve(__dirname,
+//         "./static/HTML/index.html"))
+// })
+// //Handling all errors using middleware
+// app.use(ErrorHandling)
+// app.listen(port, ()=>{
+//     console.log(`The server is running on port ${port}`);
+// })
