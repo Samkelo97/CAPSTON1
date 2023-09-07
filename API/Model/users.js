1b07f042975c3ff3e2e49747418af88f93ae44d9
Samkelo <sammpokela@gmail.com>
2023-09-07 12:20:04 +0200

    update

--- API/Model/users.js
+++ API/Model/users.js
@@ -1,6 +1,6 @@
 const db = require("../config/index"); //this imprt the db con from config
 const { hash, compare, hashSync } = require("bcrypt");
-const { createToken } = require("../Middleware/AuthenticateUser");
+const { createToken } = require("../Middleware/authenticateUser");
 class Users {
   fetchUsers(req, res) {
     const query = `
