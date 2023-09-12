0915a8edcf77ce7fc353bb317027e8b9e01e2c33
Samkelo <sammpokela@gmail.com>
2023-09-12 15:52:29 +0200

    new

--- API/Model/users.js
+++ API/Model/users.js
@@ -1,11 +1,12 @@
-const db = require("../config") //this imprt the db con from config
+const db = require("../config")
 const {hash, compare, hashSync} = require('bcrypt')
 const {createToken} = require('../Middleware/authentication')
-class Users{
+class users{
     fetchUsers(req, res){
         const query = `
-        SELECT userID, firstName, lastName, userAge, Gender, userRole,
-        emailAdd, userProfile
+        SELECT userID, firstName, lastName,
+        gender, userAge, userRole, emailAdd,
+        userProfile
         FROM Users;
         `
         db.query(query,(err, results)=>{
@@ -18,10 +19,11 @@ class Users{
     }
     fetchUser(req, res){
         const query = `
-        SELECT userID, firstName, lastName, userAge, Gender, userRole,
-        emailAdd,userProfile
+        SELECT userID, firstName, lastName,
+        gender, userAge, userRole, emailAdd,
+        userProfile
         FROM Users
-        WHERE userId = ${req.params.id};
+        WHERE userID = ${req.params.id};
         `
         db.query(query,
             (err, result)=>{
@@ -32,69 +34,72 @@ class Users{
                 })
             })
     }
-    async register(req, res) {
-      const data = req.body;
-      // encrypt password
-      data.userPass = await hash(data.userPass, 4)
-      // Payload
-      const user = {
-        emailAdd: data.emailAdd,
-        userPass: data.userPass,
-      };
-      // Query
-      const query = `INSERT INTO Users SET ?;`;
-  
-      db.query(query, [data], (err) => {
-        if (err) throw err;
-        let token = createToken(user);
-        res.json({
-          status: res.statusCode,
-          msg: "You are now registered.",
-          token,
-        });
-      });
+    login(req, res){
     }
-
-    login(req, res) {
-      const { emailAdd, userPass } = req.body;
-      const query = `
-      SELECT userID,firstName, lastName,
-      gender, userAge, userRole, emailAdd, userPass,
-      userProfile
-      FROM Users
-      WHERE emailAdd = '${emailAdd}';
-      `;
-      db.query(query, async (err, result) => {
-          if (err) throw err;
-          if (!result?.length) {
+    async register(req, res){
+        const data = req.body
+        //encrypt password
+        data.userPass = await hash(data.userPass, 15)
+        //PAYLOAD means DATA THAT COMES FROM THE USER
+        const user = {
+          emailAdd: data.emailAdd,
+          userPass: data.userPass
+        }
+        //query
+        const query = `
+        INSERT INTO Users
+        SET ?;
+        `
+        db.query(query,[data], (err)=>{
+          if(err) throw err
+          //create a token
+          let token = createToken(user)
               res.json({
                   status: res.statusCode,
-                  msg: "You provided a wrong email."
-              });
-          } else {
-              await compare(userPass, result[0].userPass, (cErr, cResult) => {
-                  if (cErr) throw cErr;
-                  // Create a token
-                  const token = createToken({
-                      emailAdd,
-                      userPass
-                  })
-                  if (cResult) {
-                      res.json({
-                          msg: "Logged in",
-                          token,
-                          result: result[0]
-                      });
-                  } else {
-                      res.json({
-                          status: res.statusCode,
-                          msg: "Invalid password or you have not registered"
-                      });
-                  }
-              });
-          }
-      });
-  }
+                  msg: "You are now registered."
+              })
+        })
+      }
+    login(req, res) {
+        const { emailAdd, userPass } = req.body;
+        const query = `
+        SELECT userID,firstName, lastName,
+        gender, userAge, userRole, emailAdd, userPass,
+        userProfile
+        FROM Users
+        WHERE emailAdd = '${emailAdd}';
+        `;
+        db.query(query, async (err, result) => {
+            if (err) throw err;
+            if (!result?.length) {
+                res.json({
+                    status: res.statusCode,
+                    msg: "You provided a wrong email."
+                });
+            } else {
+                await compare(userPass, result[0].userPass, (cErr, cResult) => {
+                    if (cErr) throw cErr;
+                    // Create a token
+                    const token = createToken({
+                        emailAdd,
+                        userPass
+                    })
+                    if (cResult) {
+                        res.json({
+                            msg: "Logged in",
+                            token,
+                            result: result[0]
+                        });
+                    } else {
+                        res.json({
+                            status: res.statusCode,
+                            msg: "Invalid password or you have not registered"
+                        });
+                    }
+                });
+            }
+        });
+    }
     updateUser(req, res){
         const data = req.body
         if (data.userPass){
@@ -102,13 +107,12 @@ class Users{
             hashSync(data.userPass, 15)
         }
         const query =`
-        UPDATE User
+        UPDATE Users
         SET ?
         WHERE userID = ?
         `
         db.query(query,
             [req.body, req.params.id],
-            
             (err)=>{
                 if(err) throw err
                 res.json({
@@ -130,4 +134,4 @@ class Users{
         })
     }
 }
-module.exports = Users
+module.exports = users
