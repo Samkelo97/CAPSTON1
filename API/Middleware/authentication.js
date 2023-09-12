b443fb5abe1e2901cde4bb439c5938e1aece57be
Samkelo <sammpokela@gmail.com>
2023-09-12 16:23:12 +0200

    update

--- API/Middleware/authentication.js
+++ API/Middleware/authentication.js
@@ -1,24 +1,59 @@
+// const {sign, verify} = require('jsonwebtoken')
+// require("dotenv").config()
+// function createToken(user) {
+//     return sign({
+//         emailAdd: user.emailAdd,
+//         userPass: user.userPass
+//     },
+//     process.env.SECRET_KEY,
+//     {
+//         expiresIn: '1h'
+//     })
+// }
+// function verifyAToken(req, res, next){
+//     /*
+//     To prevent undefined error, place ?. before your property.
+//     */
+//    try{
+//         // Retrieve token from req.headers
+//         console.log("Get token from req.headers['authorization']");
+//         const token = req.headers["authorization"]
+//         console.log(token);
+//         next()
+//    }catch(e){
+//         res.json({
+//             status: res.statusCode,
+//             msg: e.message
+//         })
+//    }
+// }
+// module.exports = {
+//  createToken,
+//  verifyAToken
+// }
+// // function verifyAToken(req, res, next){
+// //   const token = req.headers["authorization"].
+// // }
 const {sign, verify} = require('jsonwebtoken')
 require("dotenv").config()
+
 function createToken(user) {
     return sign({
         emailAdd: user.emailAdd,
-        userPass: user.userPass
+        userPass: user.userPass,
+        // authenticated: authenticated
     },
     process.env.SECRET_KEY,
     {
         expiresIn: '1h'
-    })
+    })  
 }
+
 function verifyAToken(req, res, next){
-    /*
-    To prevent undefined error, place ?. before your property.
-    */
    try{
-        // Retrieve token from req.headers
-        console.log("Get token from req.headers['authorization']");
+        // console.log("Get token from req.headers['authorization']");
         const token = req.headers["authorization"]
-        console.log(token);
+        // console.log(token);
         next()
    }catch(e){
         res.json({
@@ -27,10 +62,9 @@ function verifyAToken(req, res, next){
         })
    }
 }
+
 module.exports = {
- createToken,
- verifyAToken
+    createToken,
+    verify,
+    verifyAToken
 }
-// function verifyAToken(req, res, next){
-//   const token = req.headers["authorization"].
-// }
