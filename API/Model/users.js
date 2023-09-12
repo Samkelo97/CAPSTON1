9fa77a7019083042a174a14dd466d5bcda707fba
Samkelo <sammpokela@gmail.com>
2023-09-12 14:53:31 +0200

    update

--- API/Model/users.js
+++ API/Model/users.js
@@ -1,163 +1,140 @@
-const db = require("../config");
-const { hash, compare } = require("bcrypt");
-const { createToken } = require("../Middleware/authentication");
-
-class Users {
-  fetchUsers(req, res) {
-    const query = `
-      SELECT userID, firstName, lastName, userAge, Gender, userRole,
-      emailAdd, userProfile
-      FROM Users;
-    `;
-    db.query(query, (err, results) => {
-      if (err) throw err;
-      res.json({
-        status: res.statusCode,
-        results,
-      });
-    });
-  }
-
-  fetchUser(req, res) {
-    const query = `
-      SELECT userID, firstName, lastName, userAge, Gender, userRole,
-      emailAdd, userProfile
-      FROM Users
-      WHERE userID = ?;
-    `;
-    db.query(query, [req.params.id], (err, result) => {
-      if (err) throw err;
-      res.json({
-        status: res.statusCode,
-        result,
-      });
-    });
-  }
-
-  async register(req, res) {
-    const data = req.body;
-    // Hash the password before storing it
-    data.userPass = await hash(data.userPass, 15);
-
-    // Create a user object for token generation
-    const user = {
-      emailAdd: data.emailAdd,
-      userPass: data.userPass,
-    };
-
-    // SQL query to insert user data
-    const query = `
+const db = require("../config") //this imprt the db con from config
+const {hash, compare, hashSync} = require('bcrypt')
+const {createToken} = require('../Middleware/authentication')
+class Users{
+    fetchUsers(req, res){
+        const query = `
+        SELECT userID, firstName, lastName, userAge, Gender, userRole,
+        emailAdd, userProfile
+        FROM Users;
+        `
+        db.query(query,(err, results)=>{
+            if(err)throw err
+            res.json({
+                status: res.stausCode,
+                results
+            })
+        })
+    }
+    fetchUser(req, res){
+        const query = `
+        SELECT userID, firstName, lastName, userAge, Gender, userRole,
+        emailAdd,userProfile
+        FROM Users
+        WHERE userId = ${req.params.id};
+        `
+        db.query(query,
+            (err, result)=>{
+                if (err) throw err
+                res.json({
+                    status: res.statusCode,
+                    result
+                })
+            })
+    }
+    login(req, res){
+    }
+     async register(req, res){
+      const data = req.body
+      //encrypt password
+      data.userPass = await hash(data.userPass, 15)
+      //PAYLOAD means DATA THAT COMES FROM THE USER
+      const user = {
+        emailAdd: data.emailAdd,
+        userPass: data.userPass
+      }
+      //query
+      const query = `
       INSERT INTO Users
       SET ?;
-    `;
-
-    db.query(query, [data], (err) => {
-      if (err) throw err;
-
-      // Create a token
-      const token = createToken(user);
-
-      res.json({
-        status: res.statusCode,
-        msg: "You are now registered.",
-        token,
-      });
-    });
-  }
-
-  async login(req, res) {
-    const { emailAdd, userPass } = req.body;
-
-    const query = `
-      SELECT userID, firstName, lastName, userAge, Gender, userRole,
-      emailAdd, userProfile, userPass
-      FROM Users
-      WHERE emailAdd = ?;
-    `;
-
-    db.query(query, [emailAdd], async (err, result) => {
-      if (err) throw err;
-
-      if (!result?.length) {
-        res.json({
-          status: res.statusCode,
-          msg: "You provided a wrong email.",
-        });
-      } else {
-        const storedHashedPassword = result[0].userPass;
-
-        try {
-          const passwordMatch = await compare(userPass, storedHashedPassword);
-
-          if (passwordMatch) {
-            // Create a token
-            const token = createToken({
-              emailAdd,
-              userPass,
-            });
-
+      `
+      db.query(query,[data], (err)=>{
+        if(err) throw err
+        //create a token
+        let token = createToken(user)
+            // res.cookie("LegitUser", token, {
+            //     maxAge:  3600000,
+            //     httpOnly: true
+            // });
             res.json({
-              msg: "Logged in",
-              token,
-              result: result[0],
-            });
-          } else {
-            res.json({
-              status: res.statusCode,
-              msg: "Invalid password or you have not registered",
-            });
-          }
-        } catch (error) {
-          console.error("Error comparing passwords:", error);
-          res.json({
-            status: res.statusCode,
-            msg: "Error comparing passwords",
-          });
-        }
-      }
-    });
-  }
-
-  async updateUser(req, res) {
-    const data = req.body;
-    if (data.userPass) {
-      data.userPass = await hash(data.userPass, 15); // Hash the password before updating
+                status: res.statusCode,
+                msg: "You are now registered."
+            })
+      })
     }
-    const query = `
-      UPDATE Users
-      SET ?
-      WHERE userID = ?
-    `;
-    db.query(
-      query,
-      [data, req.params.id],
-      (err) => {
-        if (err) throw err;
-        res.json({
-          status: res.statusCode,
-          msg: "The user record was updated.",
+    login(req, res) {
+        const { emailAdd, userPass } = req.body;
+        // Use parameterized query
+        const query = `
+        SELECT userID, firstName, lastName, userAge, Gender, userRole,
+        emailAdd,userProfile
+        FROM Users
+        WHERE emailAdd = '${emailAdd}';
+        `;
+        db.query(query,[emailAdd] ,async (err, result) => {
+            if (err) throw err;
+            if (!result?.length) {
+                res.json({
+                    status: res.statusCode,
+                    msg: "You provided a wrong email."
+                });
+            } else {
+                await compare(userPass, result[0].userPass, (cErr, cResult) => {
+                    if (cErr) throw cErr;
+                    // Create a token
+                    const token = createToken({
+                        emailAdd,
+                        userPass
+                    })
+                    if (cResult) {
+                        res.json({
+                            msg: "Logged in",
+                            token,
+                            result: result[0]
+                        });
+                    } else {
+                        res.json({
+                            status: res.statusCode,
+                            msg: "Invalid password or you have not registered"
+                        });
+                    }
+                });
+            }
         });
-      }
-    );
-  }
-
-  deleteUser(req, res) {
-    const query = `
-      DELETE FROM Users
-      WHERE userID = ?;
-    `;
-    db.query(query, [req.params.id], (err) => {
-      if (err) throw err;
-      res.json({
-        status: res.statusCode,
-        msg: "A user record was deleted.",
-      });
-    });
-  }
-
-  // Other methods...
-
-  // You can add more methods like fetchProducts, addProduct, etc.
+    }
+    updateUser(req, res){
+        const data = req.body
+        if (data.userPass){
+            data.userPass =
+            hashSync(data.userPass, 15)
+        }
+        const query =`
+        UPDATE User
+        SET ?
+        WHERE userID = ?
+        `
+        db.query(query,
+            [req.body, req.params.id],
+            
+            (err)=>{
+                if(err) throw err
+                res.json({
+                    status: res.statusCode,
+                    msg: "The user record was updated."
+                })
+            })
+    }
+    deleteUser(req, res){
+        const query = ` DELETE FROM Users
+        WHERE userID = ${req.params.id};
+        `
+        db.query(query,(err)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "A user recored was deleted."
+            })
+        })
+    }
 }
-
-module.exports = Users;
+module.exports = Users
