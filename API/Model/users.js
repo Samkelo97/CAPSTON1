d55c97d981319eef06bde1576d3a4278a87b820f
Samkelo <sammpokela@gmail.com>
2023-09-13 09:53:50 +0200

    update

--- API/Model/users.js
+++ API/Model/users.js
@@ -11,7 +11,7 @@ class Users{
         db.query(query,(err, results)=>{
             if(err)throw err
             res.json({
-                status: res.stausCode,
+                status: res.statusCode,
                 results
             })
         })
@@ -62,46 +62,60 @@ class Users{
         })
       })
   }
-
-    login(req, res) {
-      const { emailAdd, userPass } = req.body;
-      const query = `
-      SELECT userID,firstName, lastName,
+  login(req, res) {
+    const { emailAdd, userPass } = req.body;
+    const query = `
+      SELECT userID, firstName, lastName,
       gender, userAge, userRole, emailAdd, userPass,
       userProfile
       FROM Users
       WHERE emailAdd = '${emailAdd}';
-      `;
-      db.query(query, async (err, result) => {
-          if (err) throw err;
-          if (!result?.length) {
-              res.json({
-                  status: res.statusCode,
-                  msg: "You provided a wrong email."
-              });
+    `;
+    db.query(query, async (err, result) => {
+      if (err) {
+        console.error("Error fetching user data:", err);
+        return res.status(500).json({
+          status: 500,
+          msg: "An error occurred while fetching user data."
+        });
+      }
+  
+      if (!result?.length) {
+        console.log("User not found for email:", emailAdd);
+        return res.json({
+          status: res.statusCode,
+          msg: "You provided a wrong email."
+        });
+      } else {
+        try {
+          const passwordMatch = await compare(userPass, data.userPass);
+          
+          if (passwordMatch) {
+            const token = createToken({
+              emailAdd,
+              userPass
+            });
+            res.json({
+              msg: "Logged in",
+              token,
+              result: result[0]
+            });
           } else {
-              compare(userPass, result[0].userPass, (cErr, cResult) => {
-                  if (cErr) throw cErr;
-                  // Create a token
-                  const token = createToken({
-                      emailAdd,
-                      userPass
-                  })
-                  if (cResult) {
-                      res.json({
-                          msg: "Logged in",
-                          token,
-                          result: result[0]
-                      });
-                  } else {
-                      res.json({
-                          status: res.statusCode,
-                          msg: "Invalid password or you have not registered"
-                      });
-                  }
-              });
+            console.log("Password mismatch for email:", emailAdd);
+            res.json({
+              status: res.statusCode,
+              msg: "Invalid password or you have not registered"
+            });
           }
-      });
+        } catch (error) {
+          console.error("Error comparing passwords:", error);
+          res.status(500).json({
+            status: 500,
+            msg: "An error occurred while comparing passwords."
+          });
+        }
+      }
+    });
   }
     updateUser(req, res){
         const data = req.body
